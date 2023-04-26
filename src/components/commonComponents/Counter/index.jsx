5be1636bfbd302f89bb9ff2f6ff7069ac7eaf15f
Avinash  Sharma <avinash@Avinashs-MacBook-Pro.local>
2023-04-26 09:55:46 +0530

    minnor changes

--- src/components/commonComponents/Counter/index.jsx
+++ src/components/commonComponents/Counter/index.jsx
@@ -1,4 +1,4 @@
-import { Button, HStack, IconButton, Input, useNumberInput } from "@chakra-ui/react";
+import { HStack, Icon, IconButton, Input, useNumberInput } from "@chakra-ui/react";
 import React from "react";
 import { MdOutlineAddCircleOutline, MdRemoveCircleOutline } from 'react-icons/md';
 
@@ -9,7 +9,6 @@ const index = () => {
       defaultValue: 0,
       min: 0,
       max: 20,
-      precision: 0,
     });
 
   const inc = getIncrementButtonProps();
@@ -18,9 +17,9 @@ const index = () => {
   return (
     <HStack maxW="300px">
       
-      <IconButton rounded={'full'} aria-label='add' size={'sm'}  {...inc} icon={<MdOutlineAddCircleOutline />} />
-      <Input maxW='60px' size={'sm'} {...input} />
-      <IconButton rounded={'full'} aria-label='add' size={'sm'} {...dec} icon={<MdRemoveCircleOutline />} />
+    <IconButton rounded={'full'} aria-label='add' size={'xs'} {...dec} icon={<Icon as={MdRemoveCircleOutline} w={5} h={5} />} />
+    <Input isDisabled={'true'} maxW='40px' size={'sm'} {...input} />
+    <IconButton rounded={'full'} aria-label='add' size={'xs'}  {...inc} icon={<Icon as={MdOutlineAddCircleOutline} w={5} h={5} />} />
      
     </HStack>
   );
